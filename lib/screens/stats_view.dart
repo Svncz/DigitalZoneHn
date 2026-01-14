c6ce3970837897acd5d7cf22895cc614f8bbe09d
Svncz <faramircruz8@gmail.com>
2026-01-13 21:03:05 -0600

    correccion de pagina

--- lib/screens/stats_view.dart
+++ lib/screens/stats_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:intl/intl.dart';
+
 import 'package:url_launcher/url_launcher.dart';
 
 class StatsView extends StatefulWidget {
